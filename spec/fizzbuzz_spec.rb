072672c75a73f0b6f5036c19dbbdbfca20dec8be
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-04-19 23:38:23 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,7 @@ require_relative '../fizzbuzz.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
 def fizzbuzz(x)
-  if 3 % 9 == 0
+  if x % 9 == 3
     puts "Fizz"
 end
 
